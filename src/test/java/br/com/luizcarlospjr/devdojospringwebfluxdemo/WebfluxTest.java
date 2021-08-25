6c2749aead225da0156c51375112dac4d6e76442
Luiz Carlos Pinto Júnior <luiz.ilegra@terceiros.zenvia.com>
2021-08-25 17:07:01 -0300

    Test for blockhound usage

---
+++ src/test/java/br/com/luizcarlospjr/devdojospringwebfluxdemo/WebfluxTest.java
@@ -0,0 +1,41 @@
+package br.com.luizcarlospjr.devdojospringwebfluxdemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public abstract class WebfluxTest {
+
+    @BeforeAll
+    public static void blockHoundSetUp() {
+        BlockHound.install();
+    }
+
+    @Test
+    @DisplayName("Given that Block Hound is installed; When call for a thread-locker process; Should throw a blocking operation error")
+    void givenThatBlockHoundIsInstalled_whenCallForAThreadLockerProcess_shouldThrowABlockingOperationError() {
+        FutureTask<String> task = new FutureTask<>(() -> {
+            Thread.sleep(0);
+            return "";
+        });
+
+        Schedulers.parallel().schedule(task);
+
+        try {
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("Should fail");
+        } catch (Exception e) {
+            assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
+}
